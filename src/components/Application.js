8c224ffe08c638dbb282cb5470c6cb04ba048192
Jaylen patterson <jaylenpatterson16@gmail.com>
2022-01-18 21:35:27 -0500

    finished button and initialized daylist item component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import "components/Application.scss";
+import 'components/Application.scss';
 
 export default function Application(props) {
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
+	return (
+		<main className="layout">
+			<section className="sidebar">
+				<img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
+				<hr className="sidebar__separator sidebar--centered" />
+				<nav className="sidebar__menu" />
+				<img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
+			</section>
+			<section className="schedule">
+				{/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+			</section>
+		</main>
+	);
 }
