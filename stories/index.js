187e79c5d391845e3abad85405a231beb23a0ec5
Jaylen patterson <jaylenpatterson16@gmail.com>
2022-01-19 17:11:03 -0500

    initialized appointment component and wrote first 2 stories for appointment

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment/index.js';
 
 storiesOf('Button', module)
 	.addParameters({
@@ -106,3 +107,10 @@ storiesOf('InterviewerList', module)
 	.add('Initial', () => <InterviewerList interviewers={interviewers} />)
 	.add('Selected', () => <InterviewerList interviewers={interviewers} value={3} />)
 	.add('Clickable', () => <InterviewerList interviewers={interviewers} onChange={action('setInterviewer')} />);
+
+storiesOf('Appointment', module)
+	.addParameters({
+		backgrounds: [ { name: 'white', value: '#fff', default: true } ]
+	})
+	.add('Appointment', () => <Appointment />);
+	.add('Appointment with Time'), () => <Appointment time="12pm"/>
